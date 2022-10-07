d61d6b1d42bb22117b17717cdb306d6061c78241
TaiwanSam <zhangchonghao416@gmail.com>
2022-10-07 19:56:22 +0800

    11

--- sam/all_function.js
+++ sam/all_function.js
@@ -1,22 +1,14 @@
-var n=0;
 function GetDate(){
     date=new Date()
     var time_detail=[date.getFullYear(),date.getMonth()+1,date.getDate(),date.getHours(),date.getMinutes(),date.getSeconds()];
     var time_format=["年","月","日","時","分","秒"];
-    var array_output="現在是"+"<br>";
+    var array_output="現在是";
     i=0;
     while(i<time_detail.length){
         array_output=array_output+time_detail[i]+time_format[i];
         i++;}
     document.getElementById("id").innerHTML=array_output;}
     
-var n=0;
-changebgcolor(){
-    var bgcolor=["red","orange","yellow","lime","green","skyblue","blue","purple","white"]
-    document.body.style.backgroundColor = bgcolor[n%9];
-    n=n+1;}
-
-
 
 
 var word="$1";
